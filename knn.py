20520705bd78922531ce0201bfe91859f5772d04
C-AJ <89857745+C-AJ@users.noreply.github.com>
2023-02-25 03:46:54 -0800

    jesus christ i hate this

--- knn.py
+++ knn.py
@@ -13,17 +13,29 @@ from sklearn.neighbors import KNeighborsClassifier
 import csv
 
 db = []
+X = []
+Y = []
+errors = 0
+total = 0
 
 #reading the data in a csv file
 with open('binary_points.csv', 'r') as csvfile:
   reader = csv.reader(csvfile)
   for i, row in enumerate(reader):
+      holder = []
       if i > 0: #skipping the header
-         db.append (row)
+         db.append(row)
+         holder.append(float(row[0]))
+         holder.append(float(row[1]))
+         if row[2] == "+":
+             Y.append(1)
+         else:
+             Y.append(2)
+         X.append(holder)
 
 #loop your data to allow each instance to be your test set
-for ? in db:
-
+for counter in range(len(X)):
+    total += 1
     #add the training features to the 2D array X removing the instance that will be used for testing in this iteration. For instance, X = [[1, 3], [2, 1,], ...]]. Convert each feature value to
     # float to avoid warning messages
     #--> add your Python code here
@@ -36,7 +48,7 @@ for ? in db:
 
     #store the test sample of this iteration in the vector testSample
     #--> add your Python code here
-    #testSample =
+    testSample = Y[counter]
 
     #fitting the knn to the data
     clf = KNeighborsClassifier(n_neighbors=1, p=2)
@@ -45,12 +57,18 @@ for ? in db:
     #use your test sample in this iteration to make the class prediction. For instance:
     #class_predicted = clf.predict([[1, 2]])[0]
     #--> add your Python code here
+    class_predicted = clf.predict([X[counter]])[0]
+    print([X[counter]])
 
     #compare the prediction with the true label of the test instance to start calculating the error rate.
     #--> add your Python code here
+    if class_predicted != testSample:
+        errors += 1
 
 #print the error rate
 #--> add your Python code here
+print("Error Rate: " + str((errors / total) * 100) + "%")
+
 
 
 
